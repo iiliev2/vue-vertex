b82e5e1a303305a73c679524c7d71ef169e777a6
iiliev2 <iiliev2@CSCBGRAH769801>
2017-06-29 14:56:53 +0300

    Fixed the production build of the ui.

--- vue-ui-client/webpack.config.js
+++ vue-ui-client/webpack.config.js
@@ -3,50 +3,48 @@ var path = require('path');
 var webpack = require('webpack');
 
 const PATHS = {
-	build : path.join(__dirname, "..", "vw-web", 'target', 'classes',
-			'WEB-INF', 'jsbundles')
+    build: path.join(__dirname, "..", "vw-web", 'target', 'classes',
+        'WEB-INF')
 };
 
 module.exports = {
-	entry : './src/main/js/index.js',
+    entry: './src/main/js/index.js',
 
-	devServer : {
-		inline : true,
-		port : 23001
-	},
+    devServer: {
+        inline: true,
+        port: 23001
+    },
 
-	output : {
-		path : PATHS.build,
-		publicPath : '/',
-		filename : 'jsbundles/app-bundle.js'
-	},
-	module : {
-		rules : [
-		// process *.vue files using vue-loader
-		{
-			test : /\.vue$/,
-			loader : 'vue-loader'
-		},
-		// process *.js files using babel-loader
-		// the exclude pattern is important so that we don't
-		// apply babel transform to all the dependencies!
-		{
-			test : /\.js$/,
-			exclude : /(node_modules|bower_components)/,
-			use : {
-				loader : 'babel-loader',
-				options : {
-					presets : [ 'es2015' ],
-					plugins : [ 'transform-runtime' ]
-				}
-			}
-		} ]
-	},
-
-	resolve : {
-		alias : {
-			vue : 'vue/dist/vue.js'
-		}
-	}
-
+    output: {
+        path: PATHS.build,
+        publicPath: '/',
+        filename: path.join('jsbundles', 'app-bundle.js')
+    },
+    module: {
+        rules: [
+            // process *.vue files using vue-loader
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // process *.js files using babel-loader
+            // the exclude pattern is important so that we don't
+            // apply babel transform to all the dependencies!
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
 };
