f3aca13c1563f402b4e734ccbe03de18a2ce63fc
ldunev <ldunev@dir.bg>
2017-07-06 18:13:50 +0300

    Fix defect with used persistence

--- vw-web/src/main/java/vw/be/server/factory/ManageUserServiceFactory.java
+++ vw-web/src/main/java/vw/be/server/factory/ManageUserServiceFactory.java
@@ -12,7 +12,7 @@ import static vw.be.server.common.IConfigurationConstants.MONGO_DB_PROVIDER;
 public class ManageUserServiceFactory {
 
     public static IManageUserService getService(Vertx vertx, JsonObject config){
-        switch (config.getString(DB_TYPE_KEY, "")) {
+        switch (config.getString(DB_TYPE_KEY, "").toUpperCase()) {
             case MONGO_DB_PROVIDER:
                 return new MongoManageUserService(vertx, config);
             default:
