f88387aa1645d473dcb1aec1a3ad5d66cb22dd4d
iiliev2 <garvandule@gmail.com>
2017-07-12 16:21:39 +0300

    Fixed the mocked db service to use a global map as otherwise different db verticles were becoming missaligned.
    Fixed the DB verticle test to test for the above described situation. Will now display more meaningfull messages.
    
    Changed the way the load testing sub app works. Will now log the number and time required to process a number of http requests.

--- vw-web/src/test/java/vw/be/server/verticle/ManageUserApplicationLoadTest.java
+++ vw-web/src/test/java/vw/be/server/verticle/ManageUserApplicationLoadTest.java
@@ -1,59 +1,97 @@
 package vw.be.server.verticle;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ManageUserApplicationLoadTest implements Runnable {
+    private static final String url = "http://localhost:23002/api/users/";
+    private static final String reportFormat = "[Total %s/%ss] [Average %s/s] [Recent throughput %s/s]";
+    private static final int threadsCount = Runtime.getRuntime().availableProcessors() * 2 - 1;
+    private static final long reportDelay = 500;
+    private static final long updateDelay = Math.round(Math.max(reportDelay * 0.75, 1.0));
+    // each thread will log here how many requests were successfully
+    // completed without errors
+    private static AtomicLong totalRequestsProcessed = new AtomicLong(0);
 
-    public static void main(String[] args) {
-        int i = 0;
-        while (i < 5) {
-            Thread t = new Thread(new ManageUserApplicationLoadTest());
-            t.start();
-            i++;
-        }
+    public static void main(String[] args) throws IOException {
+        System.out.println("Kill to quit...");
+        long i = 0;
+
+        while (i++ < threadsCount) new Thread(new ManageUserApplicationLoadTest()).start();
+
+        new Thread(() -> {
+            long requests,
+                    previousRequests = 0,
+                    startTime = System.currentTimeMillis(),
+                    reportedAt = startTime,
+                    lastReportedAt = reportedAt,
+                    average, recentThroughput;
+            double elapsedTime;
+            while (true) {
+                try {
+                    Thread.sleep(reportDelay);
+                } catch (InterruptedException e) {
+                }
+                synchronized (totalRequestsProcessed) {
+                    requests = totalRequestsProcessed.get();
+                    reportedAt = System.currentTimeMillis();
+
+                    recentThroughput = (long) ((requests - previousRequests) * 1000.0) / (reportedAt - lastReportedAt);
+                    previousRequests = requests;
+                    lastReportedAt = reportedAt;
+
+                    elapsedTime = (reportedAt - startTime) / 1000.0;
+
+                    average = Math.round(requests / elapsedTime);
+
+                    report(String.format(reportFormat, requests, elapsedTime, average, recentThroughput));
+                }
+            }
+        }).start();
     }
 
-    private void sendGet() throws Exception {
+    private static void report(String msg) {
+        StringBuilder report =
+                new StringBuilder()
+                        .append('\r')
+                        .append(msg);
 
-        String url = "http://localhost:23002/api/users/";
+        System.out.print(report);
+        System.out.flush();
+    }
 
+    private int sendGet() throws Exception {
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
         // optional default is GET
         con.setRequestMethod("GET");
+        int result = con.getResponseCode();
 
-        int responseCode = con.getResponseCode();
+        // closes the underlying socket, else we get resource leak and
+        // are unable to properly fire next requests
+        con.getInputStream().close();
 
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print result
-        System.out.println(response.toString());
+        con.disconnect();
+        return result;
     }
 
     @Override
     public void run() {
-        LocalDateTime after30seconds = LocalDateTime.now().plusSeconds(30);
-        while (LocalDateTime.now().isBefore(after30seconds)) {
+        long atStart = System.currentTimeMillis();
+        long requestsProcessed = 0;
+
+        while (true) {
             try {
-                sendGet();
+                if (sendGet() == 200) requestsProcessed++;
             } catch (Exception e) {
-                e.printStackTrace();
+            }
+            if (System.currentTimeMillis() > atStart + updateDelay) {
+                atStart += updateDelay;
+                totalRequestsProcessed.addAndGet(requestsProcessed);
+                requestsProcessed = 0;
             }
         }
     }
